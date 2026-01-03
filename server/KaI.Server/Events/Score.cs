a7c6e78ea2f527db44bbea6663c556fe9cda6d21
Max Brauer <ma.brauer@live.de>
2026-01-04 00:37:18 +0100

    server: add last command to score event

--- server/KaI.Server/Events/Score.cs
+++ server/KaI.Server/Events/Score.cs
@@ -5,18 +5,22 @@ namespace KaI.Server.Events;
 
 class Score : EventBase
 {
+    public string? LastCommand { get; set; }
+
     public long ScoreValue { get; set; }
 
     public long Combo { get; set; }
 
     public override void ReadJsonContent(JsonElement json)
     {
+        LastCommand = json.GetProperty("lastCommand").GetString();
         ScoreValue = json.GetProperty("score").GetInt64();
         Combo = json.GetProperty("combo").GetInt64();
     }
 
     protected override void WriteJsonContent(Utf8JsonWriter writer)
     {
+        writer.WriteString("lastCommand", LastCommand);
         writer.WriteNumber("score", ScoreValue);
         writer.WriteNumber("combo", Combo);
     }

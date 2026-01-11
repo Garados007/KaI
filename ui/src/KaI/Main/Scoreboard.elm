dcd7ea041c80abba3313af8748b82740feb284b2
Max Brauer <ma.brauer@live.de>
2026-01-11 12:09:17 +0100

    UI: Add scoreboard module

---
+++ ui/src/KaI/Main/Scoreboard.elm
@@ -0,0 +1,53 @@
+module KaI.Main.Scoreboard exposing (..)
+
+import Html exposing (div)
+import KaI.NetworkMsg as NetworkMsg
+import KaI.Main.Utils exposing (send)
+import Html.Attributes exposing (class)
+import Html exposing (text)
+
+type alias Model = Maybe NetworkMsg.ScoreStats
+
+type Msg
+    = None
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Nothing, send NetworkMsg.RequestHighScores )
+
+title : Model -> String
+title _ =
+    "Scoreboard"
+
+view : Model -> Html.Html Msg
+view model =
+    case model of
+        Nothing ->
+            Html.text "No score stats available."
+
+        Just stats ->
+            div [ class "scoreboard" ]
+                [ div [ class "entry"]
+                    [ div [] [ text "Today's High Score:" ]
+                    , div [] [ text <| String.fromInt stats.todayHighScore.value ]
+                    ]
+                , div [ class "entry" ]
+                    [ div [] [ text "All-time High Score:" ]
+                    , div [] [ text <| String.fromInt stats.allTimeHighScore.value ]
+                    ]
+                ]
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        None ->
+            ( model, Cmd.none )
+
+receive : NetworkMsg.RecNetworkMsg -> Model -> ( Model, Cmd Msg )
+receive networkMsg model =
+    case networkMsg of
+        NetworkMsg.RecScoreStats scoreStatsMsg ->
+            ( Just scoreStatsMsg, Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
